a939b3b02e88635c8224f8f5c81c0e6ce251d49f
yangshaoshun <yangshaoshun@gmail.com>
2015-10-24 22:15:28 +0800

    增加帮助和推出

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -11,7 +11,7 @@ import os
 filename = raw_input(">>>Input your diary name: ")
 
 #判断文件是否存在；存在，打印历史；不存在，新建。
-if os.path.exists(filename):
+if os.path.exists(filename + '.txt'):
     target = open(filename + '.txt', 'r')
     print target.read()
     target = open(filename + '.txt', 'a')
@@ -22,19 +22,31 @@ else:
 print "You'are editing the %r file." % filename
 print "Now you can write down your one-line journal."
 
-#input
-line = raw_input(">>> ")
-#记录日志时间
-wtime = time.strftime('%Y-%m-%d %H:%M:%S')
-
-while line != '' :    #有内容，继续；直接回车，保存退出
-    target.write(wtime + ' ' + line)
-    target.write('\n')
+while True:
     line = raw_input(">>> ")
+    if line == 'q' or line == 'quit' or line == '':
+        target.close()
+        print "保存文件并推出..."
+        break
+    elif line == 'h':
+        print '''
+        按 q/quit/Enter 推出
+        按 h 查看帮助
+        '''
+    else:
+	    wtime = time.strftime('%Y-%m-%d %H:%M:%S')
+	    target.write(wtime + ' ' + line + '\n')
+	    target.close()
 
-#save file
-print "保存文件..."
-target.close()
 
-if __name__ == '__main__':
-	main()
+
+
+#while (line != '') and (line != 'q') and (line !='quit'):    #有内容，继续；直接回车，保存退出
+#    target.write(wtime + ' ' + line)
+#    target.write('\n')
+#    line = raw_input(">>> ")
+
+
+
+#if __name__ == '__main__':
+	#main()

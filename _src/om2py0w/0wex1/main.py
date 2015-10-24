dee8d8bcf6013b55554a2ced94980c176631d965
yangshaoshun <yangshaoshun@gmail.com>
2015-10-24 20:22:47 +0800

    精简代码

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,52 +1,37 @@
 # -*- coding: utf-8 -*-
-
+'''文件说明:极简日志系统
+作者信息：yangshaoshun
+版本自述：v1.0
+    ...
+'''
+#全局引用
 import time
 import os
 
-#script, filename = argv
-
 filename = raw_input(">>>Input your diary name: ")
 
+#判断文件是否存在；存在，打印历史；不存在，新建。
 if os.path.exists(filename):
     target = open(filename + '.txt', 'r')
     print target.read()
     target = open(filename + '.txt', 'a')
-    
 else:
 	target = open(filename + '.txt', 'a')
-
-#review the history
-#target = open(filename)
-#print target.read()
 
 #give some hint
 print "You'are editing the %r file." % filename
 print "Now you can write down your one-line journal."
 
 #input
-
-daily = raw_input(">>> ")
+line = raw_input(">>> ")
+#记录日志时间
 wtime = time.strftime('%Y-%m-%d %H:%M:%S')
-#回车键 ''
-while daily != '' :    #有内容，继续；直接回车，保存退出
-    target.write(wtime + ' ' + daily)
+
+while line != '' :    #有内容，继续；直接回车，保存退出
+    target.write(wtime + ' ' + line)
     target.write('\n')
-    daily = raw_input(">>> ")
-    
-
-#w,wirite mode
-#r,read mode
-#a,append mode
-
-
-#write in srting and enter
-
+    line = raw_input(">>> ")
 
 #save file
 print "保存文件..."
 target.close()
-
-
-#check the file again
-#target_again = open(filename)
-#print target_again.read()

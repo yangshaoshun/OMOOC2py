a17521594f6e988b810c6e2df5318e759f883078
yangshaoshun <yangshaoshun@gmail.com>
2015-10-28 00:19:24 +0800

    各个版本的代码

--- _src/om2py0w/0wex2/main_me.py
+++ _src/om2py0w/0wex2/main_me.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 '''文件说明:极简日志系统-桌面版
 作者信息：yangshaoshun
-版本自述：v1.2
-更新：本地必须是有 mydiary.log；使用 text 插件显示内容，不用输入文件名称；main_me.py 使用 message 插件显示内容
+版本自述：v1.0
 待解决问题1：因为 idle 编码问题，无法输入中文
-待解决问题2：考虑使用 单行显示的text + message 输入和展示
+待解决问题2：对文件的判断和操作，同时提示用户
 待解决问题3：界面布局
 待解决问题4：不止输入一行字符，使用 txt 输入一段文字，甚至可以输入 md 格式，下面展示转换后的内容
 代办事项：书写 readme.md
@@ -17,7 +16,7 @@ from sys import argv
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-#script, filename = argv
+script, filename = argv
 
 #开启窗口
 master = Tk()
@@ -33,41 +32,29 @@ master.title('^--^')
 #通过按钮读取输入的内容，保存到本地文件中
 def callback(*args):   # *args 的具体含义不知道，不加的话，提示 callback() takes no arguments
     txt = v.get()
-    #target = open(filename + '.txt', 'a')
-    target = open('mydiary.log', 'a')
+    target = open(filename + '.txt', 'a')
     target.write(txt + '\n')
-    target = open('mydiary.log')
-    #s.set(target.read())
-    with open('mydiary.log') as f:
-        for line in f:
-            lastline = line
+    target = open(filename + '.txt')
+    s.set(target.read())
     e.delete(0, END)
     e.focus_set()
-    t.insert(END, lastline)
 
 #输入框
 v = StringVar()
-e = Entry(master, textvariable = v , width=50)
+e = Entry(master, textvariable = v)
 e.bind('<Return>',callback)
 e.pack()
 
-#b = Button(master, text="get", width=10, command=callback)
-#b.pack()
+b = Button(master, text="get", width=10, command=callback)
+b.pack()
 
 #读取文件内容
 s = StringVar()
-target = open('mydiary.log')
+target = open(filename + '.txt')
 s.set(target.read())
 #展示文件内容
-#w = Message(master, textvariable=s, width=50)
-#w.pack()
-
-t = Text(master)
-t.pack()
-with open('mydiary.log') as f:
-    for line in f:
-        t.insert(END, line)
-        lastline = line
+w = Message(master, textvariable=s)
+w.pack()
 
 bu = Button(master, text = 'quit', command = master.quit) #退出按钮，添加成功
 bu.pack(side = LEFT)

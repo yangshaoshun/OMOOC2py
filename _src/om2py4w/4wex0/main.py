6650e5ee2b25f26b97dcfedb8fa921ed39f1006d
yangshaoshun <yangshaoshun@gmail.com>
2015-11-12 11:39:28 +0800

    try new function

--- _src/om2py4w/4wex0/main.py
+++ _src/om2py4w/4wex0/main.py
@@ -5,6 +5,17 @@ import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
+'''
+def history():
+    open_file = open("mydaily.log")
+    history = open_file.read()
+'''
+
+def new(txt_add):
+    target = open("mydaily.log", 'a')
+    target.write(txt_add + '\n')
+    target.close()
+
 @route('/')
 @route('/text') #text 页面
 
@@ -16,9 +27,7 @@ def input(): #把text页面与 input 函数绑定，每次打开text 页面，�
 @route('/text', method ='POST')
 def do_input():
     data = request.forms.get('indata')
-    target = open("mydaily.log", 'a')
-    target.write(data + '\n')
-    target.close()
+    new(data)
     open_file = open("mydaily.log")
     history = open_file.read()
     return template('input.tpl', history = history)  

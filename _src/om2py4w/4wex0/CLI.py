7a2954ff28674eb8a93aa294ac7401f2d67ed66b
yangshaoshun <yangshaoshun@gmail.com>
2015-11-18 00:24:16 +0800

    def log to repeat use

--- _src/om2py4w/4wex0/CLI.py
+++ _src/om2py4w/4wex0/CLI.py
@@ -6,22 +6,22 @@ import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-read = requests.get('http://localhost:8080/log')
-print read.text
+def print_log():
+    read = requests.get('http://localhost:8080/log')
+    print read.text
 
 #向表单提交信息
 while True:
     line = raw_input(">>> ")
     if line in ['q','quit']:
         break
-    elif line == 'h' or line == '?' or line == 'help':
+    elif line in ['h','help','?']:
         print '''
         输入 q/quit/Enter 推出
         输入 ?/h/hlep 查看帮助
         '''
     elif line == 's':
-        read = requests.get('http://localhost:8080/log')
-        print read.text
+        print_log()
     else:
         form_data = {'indata':line}
         r = requests.post('http://localhost:8080/text',data = form_data)

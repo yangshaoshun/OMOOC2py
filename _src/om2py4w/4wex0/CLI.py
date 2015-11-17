353e0472c473e4bb0a70b13e93ce5e35094f4441
yangshaoshun <yangshaoshun@gmail.com>
2015-11-18 00:32:34 +0800

    def main()

--- _src/om2py4w/4wex0/CLI.py
+++ _src/om2py4w/4wex0/CLI.py
@@ -10,22 +10,25 @@ def print_log():
     read = requests.get('http://localhost:8080/log')
     print read.text
 
-#运行先打印日志
-print_log()
-
 #向表单提交信息
-while True:
-    line = raw_input(">>> ")
-    if line in ['q','quit']:
-        break
-    elif line in ['h','help','?']:
-        print '''
-        输入 q/quit/Enter 推出
-        输入 ?/h/hlep 查看帮助
-        '''
-    elif line == 's':
-        print_log()
-    else:
-        form_data = {'indata':line}
-        r = requests.post('http://localhost:8080/text',data = form_data)
-        continue
+def main():
+    #运行先打印日志
+    print_log()
+    while True:
+        line = raw_input(">>> ")
+        if line in ['q','quit']:
+            break
+        elif line in ['h','help','?']:
+            print '''
+            输入 q/quit/Enter 推出
+            输入 ?/h/hlep 查看帮助
+            '''
+        elif line == 's':
+            print_log()
+        else:
+            form_data = {'indata':line}
+            r = requests.post('http://localhost:8080/text',data = form_data)
+            continue
+
+if __name__ == __main__:
+    main()

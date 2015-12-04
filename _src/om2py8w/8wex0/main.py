03eeddfd0660f10b0a7400e03768f2eeb9cf4a74
yangshaoshun <yangshaoshun@gmail.com>
2015-12-04 22:04:52 +0800

    use soup success

--- _src/om2py8w/8wex0/main.py
+++ _src/om2py8w/8wex0/main.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 
 def main():
 	r = requests.get('http://esf.fang.com/house/w33/')
-	print r.text
-
+	soup = BeautifulSoup(r.text, "html.parser")
+	print (soup.title.text)
 
 if __name__ == '__main__':
     main()

5b8e25f9d09d312b1ac6ccc9fbcc632d7aa971a6
yangshaoshun <yangshaoshun@gmail.com>
2015-12-04 21:22:21 +0800

    use bootstrap framework

--- _src/om2py4w/4wex0/main.py
+++ _src/om2py4w/4wex0/main.py
@@ -21,21 +21,24 @@ def new(txt_add):
 @route('/text') #text 页面
 def input(): #把text页面与 input 函数绑定，每次打开text 页面，得到 input 函数的返回值
     log = print_log()
-    return template('input.tpl', history = log)
+    return template('in.tpl', history = log)
 
 @route('/text', method ='POST')
 def do_input():
     data = request.forms.get('indata')
     new(data)
     log = print_log()
-    return template('input.tpl', history = log)
+    return template('in.tpl', history = log)
 
 @route('/log')
 def history():
     log = print_log()
-    template = Template('{{history}}')
-    return template.render( history = log )
+    # 使用 jinja2 的模板
+#    template = Template('{{history}}')
+#    return template.render( history = log )
+    # 使用 bottle 默认的模板
 #    return template('{{history}}', history = log)
+    return template('boot.tpl', history = log)
 
 if __name__ == '__main__':
     debug(True)
---
+++ _src/om2py4w/4wex0/views/in.tpl
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Bootstrap 实例 - 基本表单</title>
+   <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css">
+   <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
+   <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form action="/text" method="post">
+<form role="form">
+   <div class="form-group">
+      <label for="name">请输入日记</label>
+      <input type="text" class="form-control" name="indata" 
+         placeholder="请输入名称">
+   </div>
+
+<form role="form">
+  <div class="form-group">
+    <label for="name">历史记录</label>
+    <textarea class="form-control" rows="20">{{history}}</textarea>
+  </div>
+</form>
+
+</body>
+</html>	
